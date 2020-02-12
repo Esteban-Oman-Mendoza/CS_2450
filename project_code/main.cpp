c2045cd856e569ff44e057000d11314e86d80795
tyaldous <tyaldous@gmail.com>
2020-02-11 19:26:37 -0700

    this is a test

--- project_code/main.cpp
+++ project_code/main.cpp
@@ -3,8 +3,10 @@
 
 /*	Program: UVSim
 	Creators: Tyler Aldous, Colby Gardiner, Esteban <Last Name>, Kolton <Last Name>, Steven Anderson.
-	Project Description: UVSim will simulates machine code for students to learn machine language and architecture. */
+	Project Description: UVSim will simulates machine code for students to learn machine language and architecture. 
+*/
 
+//This is a test to check my ability to commit and push
 
 
 int main()
